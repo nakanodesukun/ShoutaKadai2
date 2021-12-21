41e46bb71864551e8b73e40fbf26170d04d89df6
akio0911 <akio0911@gmail.com>
2021-12-21 12:12:16 +0900

    feedback

--- 2/ViewController.swift
+++ 2/ViewController.swift
@@ -13,24 +13,24 @@ class ViewController: UIViewController {
     @IBOutlet private weak var textField1: UITextField!
     @IBOutlet private weak var textField2: UITextField!
     @IBOutlet private weak var label: UILabel!
-    
+
     @IBAction func didTapButton(_ sender: Any) {
-//        入力
+        // 入力
         let text1 = Double(textField1.text ?? "") ?? 0
         let text2 = Double(textField2.text ?? "") ?? 0
         let index = segumentedControl.selectedSegmentIndex
         switch index {
         case 0:
-                label.text = "\(text1 + text2)"
+            label.text = "\(text1 + text2)"
         case 1:
             label.text = "\(text1 - text2)"
         case 2:
             label.text = "\(text1 * text2)"
         case 3:
-            if text1 != 0, text2 != 0 {
-                label.text = "\(text1 / text2)"
-            } else {
+            if text2 == 0 {
                 label.text = "0は代入できません"
+            } else {
+                label.text = "\(text1 / text2)"
             }
         default:
             print("aa")
